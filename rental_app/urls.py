479d5458a92665e35e37e50ea8aebef1189bc84c
Piyush Paul <piyushpaul605@gmail.com>
2023-11-22 21:24:43 +0530

    created create_products option

--- rental_app/urls.py
+++ rental_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home),
     path('about', views.about),
     path('create', views.create),
+    path('listing', views.listing),
+    path('createproduct', views.createproduct),
 
 ]

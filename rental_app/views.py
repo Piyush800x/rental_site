282da1d09b2d66bac711bdc520149b2c4cc87bc4
Piyush Paul <piyushpaul605@gmail.com>
2023-11-22 18:48:38 +0530

    added about

--- rental_app/views.py
+++ rental_app/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def login(request):
+    return render(request, "switch.html")
